1b9b3e0465c7598d5188bbe89e22851955374713
Everest Liu <azn_logik@yahoo.com>
2014-02-23 21:54:24 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDemoapp::Application.config.secret_key_base = '6b8e85227b6a30e99698ae9c368fe69db5462d4fbde75584dbec89bb3d72314f414bac3e9bba2ec5f204c8cd96e4e7d45acfc4f910ec7cc4a7f298c04715c63a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDemoapp::Application.config.session_store :cookie_store, key: '_rails-demoapp_session'
